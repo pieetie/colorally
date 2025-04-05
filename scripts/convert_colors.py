ff83873f2c4f1112ff701c90674b0508d04942ea
pieetie <129987442+pieetie@users.noreply.github.com>
2025-04-05 19:19:39 +0200

    script: add the color converter skeleton

---
+++ scripts/convert_colors.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3 
+
+def convert_to_protanopia(color):
+    """ 
+    """
+    pass
+
+def convert_to_deuteranopia(color):
+    """ 
+    """
+    pass 
+
+def convert_to_tritanopia(color):
+    """ 
+    """
+    pass
+
+def convert_to_grey_scale(color):
+    """ 
+    """
+    pass 
+
+def convert_colors(pair):
+    """ 
+    This function take a pair of 2 colors (list) and return a dictionnary with the HEX for the different
+    types of visions such as : 
+    - normal vision
+    - protanopia
+    - deuteranopia
+    - tritanopia
+    - grey scale
+    """
+    pass
+
