cb1f782a8d54bf6dd45cadfff10402d335cc802b
pieetie <129987442+pieetie@users.noreply.github.com>
2025-04-05 16:23:39 +0200

    script: add random color generation

---
+++ scripts/generate_colors.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import random
+
+def generate_random_color():
+    """
+    Generate a random color in a hexadecimal format
+    """
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return f'#{r:02x}{g:02x}{b:02x}'
+
+def generate_pair():
+    """
+    Generate a random pair of colors and return it as a list
+    """
+    pair = []
+    color1 = generate_random_color()
+    color2 = generate_random_color()
+    pair.append((color1, color2))
+    return pair
+
+if __name__ == "__main__":
+    print(generate_pair())
