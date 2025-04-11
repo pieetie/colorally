691e69d658a33c85dbdbe28a8a0a314453445abf
pieetie <129987442+pieetie@users.noreply.github.com>
2025-04-11 23:56:52 +0200

    update: main

--- main.py
+++ main.py
@@ -16,7 +16,27 @@ def viz_pair():
     if verify_pairs(dic1):
         #print("PASS")
         final_dic = verif_colors(dic1)
+
         print(final_dic)
+        """ 
+        Result exemple for print(final_dic)
+        {
+            'color2': {
+                'normal': '#54251e', 
+                'protanopia': '#2e2a1d', 
+                'deuteranopia': '#38321e', 
+                'tritanopia': '#612023', 
+                'grey scale': '#2e2e2e'}, 
+                
+            'color1': {
+                'normal': '#f7ba19', 
+                'protanopia': '#e4b110', 
+                'deuteranopia': '#f5c31d', 
+                'tritanopia': '#ff9d89', 
+                'grey scale': '#bbbbbb'}
+        }
+        """
+
         if final_dic:
             render_colors(final_dic)
     else: 
